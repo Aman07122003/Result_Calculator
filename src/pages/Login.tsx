04ad64ed0ba04add7aab4590580f7a885f941d57
Aman Pratap Singh <120871048+Aman07122003@users.noreply.github.com>
2025-03-13 23:31:18 +0530

    updates

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -23,27 +23,27 @@ const Login: React.FC = () => {
   };
 
   return (
-    <div className='w-full h-[100vh] bg-gray-200 flex justify-center items-center'>
-        <div className='h-[50%] md:w-[40%] w-[70%] flex items-center justify-center'>
+    <div className='w-full h-screen bg-gray-200 flex justify-center items-center'>
+        <div className='h-[50%] md:w-[40%] w-[80%] flex items-center justify-center'>
              <div className='h-[90%] w-[90%]'>
-               <h1 className='text-5xl font-bold'>Admin Login</h1>
+               <h1 className='md:text-5xl text-3xl font-bold'>Admin Login</h1>
                     {error ? <p className='p-3 bg-red-400 my-2'>{error}</p> : null}
                     <form onSubmit={handleSubmit} className='w-full flex flex-col py-4'>
                         <input
                             onChange={(e) => setEmail(e.target.value)}
-                            className='p-3 my-6 rounded-md border-[1px]'
+                            className='md:p-3 md:my-6 p-1 my-2 rounded-md border-[1px]'
                             type='email'
                             placeholder='Email'
                             autoComplete='email'
                         />
                         <input
                             onChange={(e) => setPassword(e.target.value)}
-                            className='p-3 my-5 rounded-md border-[1px]'
+                            className='md:p-3 md:my-5 p-1 my-5 rounded-md border-[1px]'
                             type='password'
                             placeholder='Password'
                             autoComplete='current-password'
                         />
-                        <button className='bg-black text-white py-3 my-6 rounded-md font-bold'>
+                        <button className='bg-black text-white md:py-3 md:my-6 p-1 my-3 rounded-md font-bold'>
                             Login
                         </button>
                     </form>
